eb15f89962373aa0f180611db64570fe8aeae9c8
thantsinmoe <thantsinmoe28@gmail.com>
2025-08-06 10:06:44 +0700

    modified pipeline

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -3,10 +3,8 @@ variable "ssh_public_key" {
   description = "Public SSH key content (from GitHub Secret)"
   type        = string
 }
-
 variable "key_name" {}
 variable "vpc_id" {}
-
 variable "ami_id" {}
 variable "instance_type" {
     default = "t2.micro"
